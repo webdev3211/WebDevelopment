d1cd26370878a0827f225679c3c26ff71db4cb03
shoaibh <31370134+shoaibh@users.noreply.github.com>
2019-11-05 18:57:58 +0530

    h

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+dbPassword =
+  "mongodb+srv://Shoaib:Shoaib@cluster0-7mkmd.mongodb.net/test?retryWrites=true&w=majority";
+
+module.exports = {
+  mongoURI: dbPassword,
+  secretOrKey: "secret"
+};
