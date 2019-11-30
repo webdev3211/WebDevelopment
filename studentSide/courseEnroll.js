f21721e43f459f752799adc16a9ecb95efb89841
shoaibh <31370134+shoaibh@users.noreply.github.com>
2019-11-30 17:47:50 +0530

    enroll

--- studentSide/courseEnroll.js
+++ studentSide/courseEnroll.js
@@ -10,31 +10,41 @@ const Registrations = require("../model/registrations").RegistrationModel;
 router.put("/enroll/:id", (req, res) => {
   Course.findOne({ _id: req.params.id })
     .then(course => {
-      course.studentId.unshift(req.user.id);
-      course.save().then(course => res.json(course));
+      if (course.contains(req.body.id)) {
+        console.log("already");
+      }
+      // course.studentId.unshift(req.body.id);
+      // course.save().then(course => console.log(course));
 
       // after that the profile of that student would be filled with that course id
-      Profile.findOne({ _id: req.user.id })
-        .then(profile => {
-          profile.courses.unshift(course.id);
-          console.log(profile);
-          profile.save().then(profile => res.json(profile));
+      // Profile.findOne({ _id: req.body.id })
+      //   .then(profile => {
+      //     profile.courses.unshift(course.id);
+      //     console.log(profile);
+      //     profile.save().then(profile => res.json(profile));
 
-          // after that the registration model would be filled with the course id and student id
-          registration = new Registrations({
-            studentId: req.user.id,
-            courseId: req.params.id,
-            paymentId: "",
-            amount: course.amount,
-            institute: profile.institute
-          });
-          registration
-            .save()
-            .then(reg => res.json(reg))
-            .catch(err => res.json(err));
-        })
-        .catch(err => console.log(err));
+      // after that the registration model would be filled with the course id and student id
+      // registration = new Registrations({
+      //   studentId: req.user.id,
+      //   courseId: req.params.id,
+      //   paymentId: "",
+      //   amount: course.fee,
+      //   institute: profile.institute
+      // });
+      // registration
+      //   .save()
+      //   .then(reg => res.json(reg))
+      //   .catch(err => res.json(err));
+      // })
+      // .catch(err => console.log(err));
     })
     .catch(err => res.json(err));
 });
 module.exports = router;
+
+Array.prototype.contains = function(needle) {
+  for (i in this) {
+    if (this[i] == needle) return true;
+  }
+  return false;
+};
