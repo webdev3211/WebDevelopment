c96080c39955cee81e038b67f3cdc1d9407509e1
shoaibh <31370134+shoaibh@users.noreply.github.com>
2019-12-08 02:14:57 +0530

    file upload complete

--- model/Material.js
+++ model/Material.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const MaterialSchema = new mongoose.Schema({
-    course : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'course',
-        required:true
-    } ,
-    materialFile : {
-        type: String,
-        unique: true
-    }
+  course: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "course",
+    required: true
+  },
+  materialFile: {
+    type: String,
+    unique: true
+  }
 });
 
-module.exports = mongoose.model('material', MaterialSchema);
+module.exports = mongoose.model("material", MaterialSchema);
